7be3158c7f798aae322d6f243e695802d3045d76
IcedCrescent <35650172+IcedCrescent@users.noreply.github.com>
2020-06-12 20:22:28 +0700

    add 3 mindmaps, iframe in index.html and handle when moving the slider

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var val = document.getElementById("valR").value;
-document.getElementById("range").innerHTML = val;
-document.getElementById("img").src = val + ".jpg";
+// This array holds the sub-websites, which are the mindmaps
+let mindmaps = ['mind map 1.html', 'mind map 2.html', 'mind map 3.html']
 
-function showVal(newVal) {
-    document.getElementById("range").innerHTML = newVal;
-    document.getElementById("img").src = newVal + ".jpg";
+let slider = document.getElementById('valR');
+let frame = document.getElementById('mindmap');
+
+// initialize the min and max value of the slider
+// based on the number of mindmaps element
+slider.min = 0;
+slider.max = mindmaps.length - 1;
+// initialize the source of the iframe to the first mindmap
+frame.src = mindmaps[0];
+// when the slider changes, change the mindmap based on the slider's value
+slider.onchange = function() {
+    frame.src = mindmaps[slider.value];
 }
